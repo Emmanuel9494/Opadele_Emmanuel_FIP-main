f77157905628ad47137c976613b91fa0a92cea4f
CrytoGod <145210773+CrytoGod@users.noreply.github.com>
2024-04-13 14:42:27 -0400

    collections
    collections is responsive but not functional will make dynamic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
-(function(){
-		
-	console.log("Js Connected");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Js Connected");
 
-	let button = document.querySelector("#button img");
+    let button = document.querySelector("#button img");
     let burgerShow = document.querySelector("#burger-con");
 
-	function hamburgerMenu() {
+    function hamburgerMenu() {
         burgerShow.classList.toggle("opaque-toggle");
-		button.classList.toggle("burger");
-	};
+        button.classList.toggle("burger");
+    };
 
-	
-
-	button.addEventListener("click", hamburgerMenu, false);		
-})();
+    if (button && burgerShow) {
+        button.addEventListener("click", hamburgerMenu, false);
+    } else {
+        console.error("Button or burgerShow element not found.");
+    }
+});
 
 // This function is for the video to play on page load and loop 
 window.onload = function() {
-	var video = document.getElementById("myVideo");
-	video.play();
-  };
-//   
+    var video = document.getElementById("myVideo");
+    if (video) {
+        video.play();
+        video.addEventListener('ended', function() {
+            this.currentTime = 0;
+            this.play();
+        }, false);
+    } else {
+        console.error("Video element not found.");
+    }
+};
